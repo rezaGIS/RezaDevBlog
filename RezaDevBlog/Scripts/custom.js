132acd1f398aeb8dcf3c6831d1c9c84809112125
Kass Rezagholi <rezaGIS@users.noreply.github.com>
2017-12-27 21:53:36 -0700

    Add files via upload
    Initial upload

---
+++ RezaDevBlog/Scripts/custom.js
@@ -0,0 +1,10 @@
+﻿//Stack pills work around
+$('#navbar').on('show.bs.collapse', function () {
+    $('.nav-pills').removeClass('nav-stacked');
+    $('.nav-pills').addClass('nav-stacked');
+    
+});
+//Remove stack pills work around
+$('#navbar').on('hide.bs.collapse', function () {
+    $('.nav-pills').removeClass('nav-stacked');
+});
